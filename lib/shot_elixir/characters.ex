e7c0aac7146b9205d6ea9ffa5dc566fa7d2086a2
Isaac Priestley <progressions@gmail.com>
2025-12-23 12:52:07 -0600

    Fix faction/juncture not returned in character update response (#150)
    ## Summary
    - After updating a character, reload it via `get_character` to ensure
    all associations (faction, juncture, user) are properly preloaded in the
    API response
    - Fixes the bug where the faction/juncture dropdown would clear after
    first selection
    
    ## Test plan
    - [ ] Update a character's faction via the dropdown
    - [ ] Verify the API response includes the faction object (not null)
    - [ ] Verify the dropdown displays the selected faction immediately
    
    Fixes Fizzy #82
    
    ---------
    
    Co-authored-by: Isaac Priestley <isaac.priestley@usehatchapp.com>

--- lib/shot_elixir/characters.ex
+++ lib/shot_elixir/characters.ex
@@ -511,16 +511,21 @@ defmodule ShotElixir.Characters do
     |> Multi.run(:broadcast, fn _repo, %{character: character} ->
       # Preload associations before broadcasting
       character_with_associations =
-        Repo.preload(character, [:user, :faction, :juncture, :image_positions])
+        Repo.preload(character, [:user, :faction, :juncture, :image_positions], force: true)
 
       broadcast_change(character_with_associations, :update)
       {:ok, character}
     end)
     |> Repo.transaction()
     |> case do
-      {:ok, %{character: character}} -> {:ok, character}
-      {:error, :character, changeset, _} -> {:error, changeset}
-      {:error, _step, reason, _changes} -> {:error, reason}
+      {:ok, %{character: character}} ->
+        {:ok, get_character(character.id) || character}
+
+      {:error, :character, changeset, _} ->
+        {:error, changeset}
+
+      {:error, _step, reason, _changes} ->
+        {:error, reason}
     end
   end
 
