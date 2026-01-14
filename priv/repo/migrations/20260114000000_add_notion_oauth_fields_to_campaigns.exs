4ab50257ded1a1d38c9cf8dd51737d3e5f388790
Isaac Priestley <progressions@gmail.com>
2026-01-14 08:37:34 -0500

    fix: add migration for notion_oauth fields on campaigns table

---
+++ priv/repo/migrations/20260114000000_add_notion_oauth_fields_to_campaigns.exs
@@ -0,0 +1,11 @@
+defmodule ShotElixir.Repo.Migrations.AddNotionOAuthFieldsToCampaigns do
+  use Ecto.Migration
+
+  def change do
+    alter table(:campaigns) do
+      add :notion_database_ids, :map, default: "{}"
+      add :notion_access_token, :string
+      add :notion_workspace_name, :string
+    end
+  end
+end
