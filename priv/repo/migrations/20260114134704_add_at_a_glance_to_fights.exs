f450e19960716da8473ca0a012c518c0f1de0712
Isaac Priestley <progressions@gmail.com>
2026-01-14 13:30:10 -0500

    fix: remove duplicate at_a_glance migration
    The at_a_glance column for fights already exists in the database
    schema (added as part of structure.sql). Removing the duplicate
    migration that causes CI to fail.

--- priv/repo/migrations/20260114134704_add_at_a_glance_to_fights.exs
+++
@@ -1,9 +0,0 @@
-defmodule ShotElixir.Repo.Migrations.AddAtAGlanceToFights do
-  use Ecto.Migration
-
-  def change do
-    alter table(:fights) do
-      add :at_a_glance, :boolean, default: false, null: false, if_not_exists: true
-    end
-  end
-end
